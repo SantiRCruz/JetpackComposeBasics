76aa5d5a236c9ac1680f9d1641a1ce977b0033f0
SantiRCruz <davidsantiagocruz02@gmail.com>
2023-10-13 19:41:44 -0500

    motion layout

--- app/src/main/java/com/example/firstcomposesteps/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposesteps/MainActivity.kt
@@ -22,24 +22,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
 import com.example.firstcomposesteps.ui.theme.FirstComposeStepsTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,71 +59,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirstComposeStepsTheme {
-                val windowInfo = rememberWindowInfo()
-                if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        items(10) {
-                            Text(
-                                text = "Item $it",
-                                fontSize = 25.sp,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(Color.Cyan)
-                                    .padding(16.dp)
-                            )
-                        }
-
-                        items(10) {
-                            Text(
-                                text = "Item $it",
-                                fontSize = 25.sp,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(Color.Green)
-                                    .padding(16.dp)
-                            )
-                        }
+                Column {
+                    var progress by remember {
+                        mutableStateOf(0f)
                     }
-                } else {
-                    Row(
-                        modifier = Modifier.fillMaxWidth()
+                    ProfileHeader(progress = progress)
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Slider(
+                        value = progress, onValueChange = {
+                            progress = it
+                        }, modifier = Modifier
+                            .padding(horizontal = 32.dp)
                     )
-                    {
-                        LazyColumn(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            items(10) {
-                                Text(
-                                    text = "Item $it",
-                                    fontSize = 25.sp,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background(Color.Green)
-                                        .padding(16.dp)
-                                )
-                            }
-
-                        }
-                        LazyColumn(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            items(10) {
-                                Text(
-                                    text = "Item $it",
-                                    fontSize = 25.sp,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background(Color.Cyan)
-                                        .padding(16.dp)
-                                )
-                            }
-
-                        }
-                    }
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun ProfileHeader(progress: Float) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources.openRawResource(R.raw.motion_scene).readBytes().decodeToString()
+    }
+    MotionLayout(
+        motionScene = MotionScene(motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val properties = motionProperties(id = "profile_pic")
+        val changedColor = properties.value.color("background")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .layoutId("box")
+        )
+        Image(
+            painter = painterResource(id = R.drawable.picture),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = changedColor,
+                    shape = CircleShape
+                )
+                .layoutId("profile_pic")
+        )
+        Text(
+            text = "Santiago Rodriguez",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .layoutId("username"),
+            color = changedColor,
+        )
     }
 }
